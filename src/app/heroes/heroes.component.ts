94f34a07d085fea7fbfe4ca1b4251e858165eead
Sun Seung-hoon <sun87066@naver.com>
2018-08-14 00:42:21 +0900

    binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  isSpecial = true;
+
+
+  hero: Hero = {  // 리터럴 객체를 사용한 방법, hero의 타입은 Hero이다.
+    id: 1,
+    name:  'winstorm'
+  };
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onSave(event: any) {
+    console.log(event);
+  }
 }
+
